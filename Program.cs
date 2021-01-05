0841e905548043ecc0f5b185d10f675ba7074e7e
Villanite <ariel@villanea.com>
2021-01-05 02:21:31 -0500

    Adding support for readline and copy paths.

--- Program.cs
+++ Program.cs
@@ -8,8 +8,13 @@ namespace File_Deduper
     {
         static void Main(string[] args)
         {
-            string filesCurrentLocation = @"C:\Users\ariel\Music\Monstercat Music\Dubstep";
-            string filesMoveToLocation = @"C:\Users\ariel\Music\Monstercat Music\Dubstep\Dupes";
+            Console.WriteLine("Please enter the Current Location of the files:");
+            string filesCurrentLocation = Console.ReadLine();
+            Console.WriteLine();
+           
+            Console.WriteLine("Please enter the Location you wish to move the duplicate files to:");
+            string filesMoveToLocation = Console.ReadLine();
+            Console.WriteLine();
 
             //Check to see if the folders even exist.
             if (!Directory.Exists(filesCurrentLocation))
@@ -21,11 +26,22 @@ namespace File_Deduper
 
             foreach (string filePath in filesToMove)
             {
+
+                var nonCopyExists = false;
+                if (filePath.Contains("- Copy"))
+                {
+                    if (Array.Exists(filesToMove, element => element == filePath.Replace(" - Copy", "")))
+                    {
+                        nonCopyExists = true;
+                    }
+                }
+
+
                 string lastTenCharactersOfFile = filePath.Substring(filePath.Length - 10, 10);
 
                 var regex = new Regex(@"( - Copy )?(\(\d{1,3}\))(.mp3)$");
 
-                if (regex.IsMatch(lastTenCharactersOfFile))
+                if (regex.IsMatch(lastTenCharactersOfFile) || nonCopyExists)
                 {
                     Console.WriteLine("Now Moving " + Path.GetFileName(filePath));
                     File.Move(filePath, filesMoveToLocation + @"\" + Path.GetFileName(filePath));
